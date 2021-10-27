ea33f6aa65aba633a518530ee402b5f01f851b42
Konstantin Karpov <77498473+dedOfficial@users.noreply.github.com>
2021-10-27 13:14:27 +0300

    Complete navbar

---
+++ js/components/navbar.js
@@ -0,0 +1,7 @@
+export default function navbar() {
+  document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelectorAll('.target').forEach((item) => {
+      item.classList.toggle('change');
+    });
+  });
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import section1 from './components/section-1.js';
+import navbar from './components/navbar.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   section1();
+  navbar();
 });
